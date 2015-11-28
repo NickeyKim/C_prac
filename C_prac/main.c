d9e990cc0c80f4f423f1172ea1d76682c88fd6a7
NAKHOE KIM <nakhoekim@nakhoes-macbook-pro.local>
2015-11-28 15:53:05 +0900

    Initial Commit

---
+++ C_prac/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_prac
+//
+//  Created by NAKHOE KIM on 11/28/15.
+//  Copyright © 2015 NAKHOE KIM. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
